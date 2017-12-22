99f04fb48c0d92154de9ef8ee094ce198119720c
jswizzy <smithjustinn@hotmail.com>
2017-12-22 15:24:32 -0500

    add todo app and unit and intergration tests

---
+++ src/main/java/com/spring/bootexample/controller/TodoController.java
@@ -0,0 +1,39 @@
+package com.spring.bootexample.controller;
+
+import com.spring.bootexample.model.Todo;
+import com.spring.bootexample.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+public class TodoController {
+    @Autowired
+    private TodoService todoService;
+
+    @GetMapping("/users/{name}/todos")
+    public List<Todo> retrieveTodos(@PathVariable String name) {
+        return todoService.retrieveTodos(name);
+    }
+
+    @GetMapping("/users/{name}/todos/{id}")
+    public Todo retrieveTodo(@PathVariable String name, @PathVariable int id) {
+        return todoService.retrieveTodo(id);
+    }
+
+    @PostMapping("/users/{name}/todos")
+    ResponseEntity<?> add(@PathVariable String name, @RequestBody Todo todo) {
+        Todo createdTodo = todoService.addTodo(name, todo.getDesc(), todo.getTargetDate(), todo.isDone());
+        if (createdTodo == null) {
+            return ResponseEntity.noContent().build();
+        }
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(createdTodo.getId()).toUri();
+        return ResponseEntity.created(location).build();
+    }
+}
---
+++ src/main/java/com/spring/bootexample/service/TodoService.java
@@ -0,0 +1,40 @@
+package com.spring.bootexample.service;
+
+import com.spring.bootexample.model.Todo;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class TodoService {
+    private static List<Todo> todos = new ArrayList<>();
+    private static int todoCount = 3;
+
+    static {
+        todos.add(new Todo(1, "Jack", "Learn Spring MVC", new Date(), false));
+        todos.add(new Todo(2, "Jack", "Learn Struts", new Date(), false));
+        todos.add(new Todo(3, "Jill", "Learn Hibernate", new Date(), false));
+    }
+
+    public List<Todo> retrieveTodos(String user) {
+        return todos.stream()
+                .filter(todo -> todo.getUser().equals(user))
+                .collect(Collectors.toList());
+    }
+
+    public Todo addTodo(String name, String desc,
+                        Date targetDate, boolean isDone) {
+        Todo todo = new Todo(++todoCount, name, desc, targetDate, isDone);
+        todos.add(todo);
+        return todo;
+    }
+
+    public Todo retrieveTodo(int id) {
+        return todos.stream()
+                .findFirst()
+                .orElse(null);
+    }
+}
